0d0a00f58f144119ce5a296513fe73c450feab34
Parvej khan <kparvej761@gmail.com>
2022-10-07 16:56:11 +0530

    bug fix

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -288,12 +288,13 @@ const Footer = () => {
             </div>
 
             {/* form  */}
-            <form action="" className="lg:col-span-2 lg:my-5">
-              <div className=" flex justify-center items-center ">
-                <div className="md:mb-6">
-                  <input
-                    type="text"
-                    className="
+            <div>
+              <form action="" className="lg:col-span-2 lg:my-5">
+                <div className=" flex justify-center items-center ">
+                  <div className="md:mb-6">
+                    <input
+                      type="text"
+                      className="
           form-control
           block
           w-full
@@ -310,24 +311,25 @@ const Footer = () => {
           m-0
           focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
         "
-                    id="exampleFormControlInput1"
-                    placeholder="Email address"
-                  />
-                </div>
-                <div className="md:mr-auto mb-6 lg:mx-2">
-                  <button
-                    type="submit"
-                    className="inline-block px-6 py-2 border-2
+                      id="exampleFormControlInput1"
+                      placeholder="Email address"
+                    />
+                  </div>
+                  <div className="md:mr-auto mx-2">
+                    <button
+                      type="submit"
+                      className="inline-block px-6 py-2 border-2
                      border-white text-white font-medium text-xs
                       leading-tight uppercase rounded hover:bg-black 
                       hover:bg-opacity-5 focus:outline-none focus:ring-0 
                       transition duration-150 ease-in-out"
-                  >
-                    Subscribe
-                  </button>
+                    >
+                      Subscribe
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
         <div
